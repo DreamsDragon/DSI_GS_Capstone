ab339c3c91e7c195c90708331b65edc4fe62874e
karu2412 <karu2412@gmail.com>
2021-10-07 12:57:42 -0400

    allow multi column match

--- scripts/process_drug_utlization.py
+++ scripts/process_drug_utlization.py
@@ -1,8 +1,10 @@
 import pandas as pd
-
+from df_string_process import process_columns
 if __name__ == "__main__":
     from sys import argv
     from pathlib import Path
     df = pd.read_csv(Path(argv[1]))
     processed_df = df.groupby(["product_name","year","quarter"]).sum()["number_of_prescriptions"]
-    processed_df.to_csv(Path(argv[2]))
+    processed_df = processed_df.reset_index()
+    processed_df = process_columns(processed_df,["product_name"])
+    processed_df.to_csv(Path(argv[2]),index=False)
